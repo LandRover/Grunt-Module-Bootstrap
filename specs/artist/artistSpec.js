c200c5b7b553f1c3f1a9997a3cd52a62c4ca6620
Oleg <github.com@landroverz.com>
2014-10-30 18:39:55 +0200

    changed testing a bit, to setup each time new object.

--- specs/artist/artistSpec.js
+++ specs/artist/artistSpec.js
@@ -3,16 +3,21 @@ define([
 ], function(Artist) {
     'use strict';
     
+    var artist;
+    
+    beforeEach(function() {
+        artist = new Artist();
+    });
+    
+    
     describe('Artist Init', function() {
         it('it can create an Artist', function() {
-            var artist = new Artist();
             artist.setName('Bob Dylan');
             
             expect(artist.name).toBe('Bob Dylan');
         });
         
         it('it can set Artist as featured', function() {
-            var artist = new Artist();
             artist.setName('Bob Dylan').setFeatured();
             
             
